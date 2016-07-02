f4214fcd2830c6b8031754461b2b9e5bdb1b3034
Tommy Ku <zoo1929@outlook.com>
2016-07-02 23:45:36 +0800

    ADD: devise model and login page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,6 @@
-class HomeController < ActionController::Base
+class HomeController < ApplicationController
   protect_from_forgery with: :exception
+  before_action :authenticate_user!
 
   def index
   end
