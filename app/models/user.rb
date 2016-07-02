3ebfcfe16eee3fc10b1720a8a665f542bd9205ac
Tommy Ku <zoo1929@outlook.com>
2016-07-03 00:06:26 +0800

    ADD: devise-two-factor to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+  devise :two_factor_authenticatable,
+         otp_secret_encryption_key: '9a6a82ea0f1b9b3cf080938e830315c9ab934c740bcbdcd17af9dac22dfe68c9f233a8c729714db07089ca0974ce0f6f8b0787259589c7175d2b6a3ff93e11f2'
+  devise :registerable, :recoverable, 
+         :rememberable, :trackable, :validatable
 end
